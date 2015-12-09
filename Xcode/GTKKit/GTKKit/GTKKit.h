5ea0e0190e8b2ea3a5ec059991cde4cb7be0fca8
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-12-08 22:20:53 -0500

    added Xcode project

---
+++ Xcode/GTKKit/GTKKit/GTKKit.h
@@ -0,0 +1,19 @@
+//
+//  GTKKit.h
+//  GTKKit
+//
+//  Created by Alsey Coleman Miller on 12/8/15.
+//  Copyright © 2015 PureSwift. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for GTKKit.
+FOUNDATION_EXPORT double GTKKitVersionNumber;
+
+//! Project version string for GTKKit.
+FOUNDATION_EXPORT const unsigned char GTKKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GTKKit/PublicHeader.h>
+
+
